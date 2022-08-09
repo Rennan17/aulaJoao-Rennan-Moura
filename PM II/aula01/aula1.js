d0cbd02dcc85d5ec127a7d94b2b312ef51450c5f
rennan17 <rsmoura252@gmail.com>
2022-08-08 21:58:35 -0300

    Aula 01 - introdução ao Js

---
+++ PM II/aula01/aula1.js
@@ -0,0 +1,16 @@
+var a = 3; // declarando variavel com var
+let b = 4; // declarando variavel com let
+console.log('conteudo de a:',a,'conteudo de b:',b)
+
+var a = 30; // a variavel foi sobrescrita
+b = 40; // nao pe necessario colorcar let ou var para chamar a variavel novamente
+
+console.log(a,b)
+
+a = 300;
+b = 400;
+
+console.log(a,b);
+
+const c = 5;
+console.log(c);
